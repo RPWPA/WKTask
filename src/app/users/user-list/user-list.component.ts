147e5b6820090dfd12898355459f3ba3e70eecb9
RPWPA <moataz.alsayed.abdelfatah@gmail.com>
2025-03-19 01:17:20 +0200

    In user-list, added searching.

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -5,12 +5,16 @@ import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { User } from '../../types/User';
 import { UserService } from '../../core/services/user.service';
-import { finalize } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, finalize } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatTableModule } from '@angular/material/table';
 import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { Subject } from 'rxjs';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatFormField, MatFormFieldModule, MatLabel } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-user-list',
@@ -20,13 +24,22 @@ import { MatProgressSpinner } from '@angular/material/progress-spinner';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
-    MatProgressSpinner
+    MatProgressSpinner,
+    MatIcon,
+    MatLabel,
+    MatFormField,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule
   ],
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
 export class UserListComponent implements AfterViewInit, OnInit {
   private userService = inject(UserService);
+  private filterSubject = new Subject<string>();
+  filter$ = this.filterSubject.asObservable();
+
   
   displayedColumns = ['id', 'fname', 'lname', 'actions'];
   dataSource = new MatTableDataSource<User>([]);
@@ -38,6 +51,7 @@ export class UserListComponent implements AfterViewInit, OnInit {
 
   ngOnInit() {
     this.loadUsers();
+    this.setupFilter();
   }
 
   ngAfterViewInit() {
@@ -51,6 +65,29 @@ export class UserListComponent implements AfterViewInit, OnInit {
         default: return item[property as keyof User];
       }
     };
+
+    this.dataSource.filterPredicate = (data: User, filter: string) => {
+      const searchString = filter.toLowerCase();
+      return (
+        data.fname.toLowerCase().includes(searchString) ||
+        data.lname.toLowerCase().includes(searchString) ||
+        data.username.toLowerCase().includes(searchString)
+      );
+    };
+  }
+
+  private setupFilter() {
+    this.filter$.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(filterValue => {
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.filterSubject.next(filterValue);
   }
 
   private loadUsers() {
